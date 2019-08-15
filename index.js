a34c383f73460b3a6da0d661328abe0d851bb0ab
katashin <ktsn55@gmail.com>
2019-08-16 00:13:14 +0900

    feat: allow to change chunk name prefix of generated routes (#10)

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const VueAutoRoutingPlugin = require('vue-auto-routing/lib/webpack-plugin')
 
-module.exports = api => {
+module.exports = (api, options) => {
+  const opts =
+    (options.pluginOptions && options.pluginOptions.autoRouting) || {}
+
+  const pluginOptions = {
+    pages: 'src/pages',
+    nested: true
+  }
+
+  if (opts.chunkNamePrefix != null) {
+    pluginOptions.chunkNamePrefix = opts.chunkNamePrefix
+  }
+
   api.chainWebpack(webpackConfig => {
     // prettier-ignore
     webpackConfig
       .plugin('vue-auto-routing')
-        .use(VueAutoRoutingPlugin, [
-          {
-            pages: 'src/pages',
-            nested: true
-          }
-        ])
+        .use(VueAutoRoutingPlugin, [pluginOptions])
   })
 }

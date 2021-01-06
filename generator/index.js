695b89a441ee0a7cfe5fb25b87ee275d18efe3a9
ktsn <ktsn@hey.com>
2021-01-07 00:12:26 +0800

    feat: support vue 3

--- generator/index.js
+++ generator/index.js
@@ -1,10 +1,10 @@
 module.exports = (api) => {
   api.extendPackage({
     dependencies: {
-      'vue-router-layout': '^0.1.2',
+      'vue-router-layout': '^0.2.0',
     },
     devDependencies: {
-      'vue-auto-routing': '^0.6.0',
+      'vue-auto-routing': '^1.0.0',
     },
     vue: {
       pluginOptions: {
